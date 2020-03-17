f0dd6e783d55f2ebc2476fde95a65e891b8151d9
duvanjm <duvanjarin@gmail.com>
2020-03-17 22:26:09 +0000

    functions_print.c

--- functions_print.c
+++ functions_print.c
@@ -1,17 +1,5 @@
 #include "holberton.h"
 /**
- * _print_char - Prints character
- * @ap: arguments
- * Return: return the amount of characters
- */
-
-int _print_char(va_list ap)
-{
-	print_char(va_arg(ap, int));
-	return (1);
-}
-
-/**
  * print_string - Print string
  * @ap: arguments
  * Return: return the amount of characters
